319029a359d9867b70f32e3c9f0845adcd64012c
Sumeet <testersumeet88@gmail.com>
2018-07-31 01:53:13 +0530

    This is my first commit for selenium

---
+++ src/main/java/testmaven/MyFirstMaven.java
@@ -0,0 +1,12 @@
+package testmaven;
+
+public class MyFirstMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is my first Maven Program");
+		
+	}
+
+}
---
+++ src/test/java/testmaven/TestMyFirstMaven.java
@@ -0,0 +1,13 @@
+package testmaven;
+
+import org.testng.annotations.Test;
+
+public class TestMyFirstMaven {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		MyFirstMaven.main(null);
+	}
+
+}
