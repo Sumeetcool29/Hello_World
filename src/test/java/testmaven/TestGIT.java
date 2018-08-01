6e70e05823b30252665035ac8e5ac1f6bb5fa0bb
Sumeet <testersumeet88@gmail.com>
2018-08-02 00:16:26 +0530

    This is my first GIT Commit

---
+++ src/test/java/testmaven/TestGIT.java
@@ -0,0 +1,13 @@
+package testmaven;
+
+import org.testng.annotations.Test;
+
+
+public class TestGIT {
+	@Test
+	public void testgit()
+	{
+		System.out.println("This is a test for GIT Connection");
+	}
+	
+}
